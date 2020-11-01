f9a11b6397ec6828428f523640b651509c5fdc2e
John Azar <eng.johnazar@gmail.com>
2020-11-01 13:13:05 +0300

    telephoneCheck

--- Telephone Number Validator/index.js
+++ Telephone Number Validator/index.js
@@ -1,5 +1,7 @@
 function telephoneCheck(str) {
-    return true;
+    // use https://regex101.com/ to generate regexp
+    let myreg = /^(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}$/;
+    return myreg.test(str);
   }
   
   telephoneCheck("555-555-5555");
