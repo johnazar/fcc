1a3e872dd91b150b731ef5c75266eb7a4444540b
John Azar (PC) <>
2021-05-09 13:51:39 +0300

    connect to db

--- Learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker2021/server.js
+++ Learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker2021/server.js
@@ -2,13 +2,57 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
 
+//The code goes here
+// Setup connection to db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//test conection
+/* let joeDoe = new User({name: "Joe Doe 2", exercise: 'push ups', duration: 20,date:new Date("2020-02-02")});
+joeDoe.save(); */
+
+// Create Schema
+// we need a collection of exercises related to single user in users' collection
+const Schema = mongoose.Schema;
+let exerciseSchema = new Schema({
+  description: { type: String, required: true },
+  exercise: { type: String },
+  duration: Number,
+  date: String // date can be used
+});
+
+let userSchema = new Schema({
+  username: { type: String, required: true },
+  log:[exerciseSchema]
+});
+
+
+//define Schema
+let ExerciseSession = mongoose.model("Session", exerciseSchema);
+const User = mongoose.model("User", userSchema);
+
+//Handel reqs setup
 app.use(cors())
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+//POST Req
+//Get Data from POST Requests Create a New User
+app.post('/api/users', function (req, res) {
+  let result = {}
+  let newuser = new User({ username: req.body.username });
+  newuser.save((err, savedUser)=>{
+    if (err) console.log(err);
+      result['_id'] = savedUser._id;
+      result['username'] = savedUser.username;
+      res.json(result);
+  });
+  
+});
 
 
 
