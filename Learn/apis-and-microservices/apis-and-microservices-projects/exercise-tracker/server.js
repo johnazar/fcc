116ce1290060b1b5b838f64077142b917f84212c
John Azar (PC) <>
2021-05-09 12:20:41 +0300

    init connect to db

--- Learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker/server.js
+++ Learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker/server.js
@@ -1,7 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+// setup connection to db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  exercise: { type: String },
+  duration: Number,
+  date: Date
+});
+
+//define schema
+const User = mongoose.model("User", userSchema);
+//test conection
+/* let joeDoe = new User({name: "Joe Doe 2", exercise: 'push ups', duration: 20,date:new Date("2020-02-02")});
+joeDoe.save(); */
+
 
 app.use(cors())
 app.use(express.static('public'))
