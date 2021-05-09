259664340bdef31badd9628b552fd472a235b4d5
John Azar (PC) <>
2021-05-09 12:32:27 +0300

    request handle example

--- Learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker/server.js
+++ Learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker/server.js
@@ -24,15 +24,55 @@ joeDoe.save(); */
 
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+//The code goes here
+
+//POST Req
+//Get Data from POST Requests Create a New User
+app.post('/api/exercise/new-user', function (req, res) {
+  let result = {}
+  result['username'] = req.body.username;
+  let newuser = new User({ name: req.body.username });
+  newuser.save();
+  result['_id'] = newuser._id;
+  console.log(result);
+  res.json(result);
+});
+
+//Get Data from POST Requests Add exercises
+app.post('/api/exercise/add', function (req, res) {
+  let result = {}
+  result['userId'] = req.body.userId;
+  result['description'] = req.body.description;
+  result['duration'] = req.body.duration;
+  result['date'] = req.body.date;
+  res.json(result);
+});
+
+
+//GET Req
+//Get Data from get Requests
+app.get('/api/exercise/users', function(req, res) {
+  
+  User.find({}).select('name').exec(function(err, arr) {
+    if (err) throw err;
+    console.log(arr);
+    res.json(arr);
+  });
+});
 
 
 
+
+
+
+//Start listen
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
