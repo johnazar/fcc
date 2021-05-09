3c1cacb61a74b6b5151bd0efe1309ca0f20861c9
John Azar (PC) <>
2021-05-09 22:55:20 +0300

    express-fileupload

--- Learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice/server.js
+++ Learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice/server.js
@@ -1,14 +1,40 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(fileUpload());
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', function (req, res) {
+  if(!req.files) {
+    res.send({
+        status: false,
+        message: 'No file uploaded'
+    });
+  }else{
+    let upfile = req.files.upfile;
+    //send response
+    res.send({
+      status: true,
+      message: 'File is uploaded',
+      data: {
+          name: upfile.name,
+          type: upfile.mimetype,
+          size: upfile.size
+        }
+    });
+
+  }
+
+
+  
 });
 
 
